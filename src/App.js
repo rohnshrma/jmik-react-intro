8d78410b782004c69a34129ec0c8b08a07268198
rohnshrma <rohn11shrma@gmail.com>
2024-10-24 23:41:56 +0530

    props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
+// Importing React library for creating components.
 import React from "react";
+
+// Importing the Card component from the Components folder.
 import Card from "./Components/Card";
+
+// Importing a CSS file for styling the components.
 import "./style.css";
+
+// Creating a functional component named 'App'.
 const App = () => {
+  // Returning JSX, which is a mix of HTML-like syntax and JavaScript.
   return (
+    // Defining a parent 'div' with a class name 'App' for styling.
     <div className="App">
+      {/* A 'main' element with a class of 'row' for layout purposes. */}
       <main className="row">
+        {/* Rendering the Card component and passing it properties (name, email, phone, and imgURL). */}
         <Card
           name="jack sparrow"
           email="pirate@email.com"
           phone="9900909090"
           imgURL="https://variety.com/wp-content/uploads/2022/05/MCDPIOF_EC366.jpg?w=1000&h=667&crop=1"
         />
+        {/* Another Card component with different properties (name, email, phone, and imgURL). */}
         <Card
           name="johnny"
           email="johnyjohny@email.com"
@@ -22,4 +34,5 @@ const App = () => {
   );
 };
 
+// Exporting the App component as the default export so it can be imported in other files.
 export default App;
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,19 +1,30 @@
+// Importing specific icons (FaPhone, FaEnvelope) from the 'react-icons/fa' library.
 import { FaPhone, FaEnvelope } from "react-icons/fa";
 
+// Creating a functional component 'Card' that takes 'props' as an argument.
 const Card = (props) => {
+  // Logging the properties passed to this component to the console for debugging purposes.
   console.log(props);
 
+  // Returning JSX to render the card component.
   return (
+    // Parent div with a class 'Card' for styling purposes.
     <div className="Card">
+      {/* A div representing the card header, containing an image. */}
       <div className="card-header">
+        {/* Rendering an 'img' element with 'src' and 'alt' attributes based on the properties passed (imgURL and name). */}
         <img src={props.imgURL} alt={props.name} />
       </div>
+      {/* A div representing the card body. */}
       <div className="card-body">
+        {/* Displaying the name property in an 'h2' tag. */}
         <h2>{props.name}</h2>
+        {/* Displaying the email property in a 'p' tag, along with an envelope icon. */}
         <p>
           <FaEnvelope />
           {props.email}
         </p>
+        {/* Displaying the phone number in a 'p' tag, along with a phone icon. */}
         <p>
           <FaPhone /> {props.phone}
         </p>
@@ -22,4 +33,5 @@ const Card = (props) => {
   );
 };
 
+// Exporting the Card component as the default export so it can be used in other files.
 export default Card;
